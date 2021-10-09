cfe2a31ead473671248d64b67814669d71325aeb
antares0982@gmail.com <Antares0982@gmail.com>
2021-10-09 17:27:01 +0800

    :rocket: ready for test

--- autoload.js
+++ autoload.js
@@ -1,5 +1,12 @@
+/*
+ * @Author: Antares
+ * @Date: 2021-10-09 17:25:35
+ * @LastEditors: Antares
+ * @LastEditTime: 2021-10-09 17:26:48
+ * @FilePath: /live2d-widget-test/autoload.js
+ */
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/Antares0982/live2d-widget@v1.0.5/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/aLyr0/live2d-widget@v1.0.5/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -34,7 +41,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/Antares0982/live2d_api/"
+			cdnPath: "https://cdn.jsdelivr.net/gh/aLyr0/live2d_api/"
 		});
 	});
 }
